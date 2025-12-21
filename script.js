b297b59b4170d3e987db13538cc9986349901ff7
devGVee <devgvee@gmail.com>
2025-12-22 00:33:51 +0530

    Implement playRound logic and scoring

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You Win. Rock beats Scissors.");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You Win. Paper beats Rock.");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You Win. Scissors beats Paper.");
+    humanScore++;
+  } else if (computerChoice === "rock" && humanChoice === "scissors") {
+    console.log("You lose. Rock beats Scissors.");
+    computerScore++;
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    console.log("You lose. Paper beats Rock.");
+    computerScore++;
+  } else if (computerChoice === "scissors" && humanChoice === "paper") {
+    console.log("You lose. Scissors beats Paper.");
+    computerScore++;
+  }
+}
+
+playRound(humanSelection, computerSelection);
