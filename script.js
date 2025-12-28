71f96753b8e48585d217a2a9e5ae655f50ac94c8
devGVee <devgvee@gmail.com>
2025-12-28 12:28:57 +0530

    Display round results using DOM elements

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const resultsDiv = document.querySelector("#results");
+const resultPara = document.createElement("p");
+
+resultsDiv.appendChild(resultPara);
+
 function getComputerChoice() {
   return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
 
 function playRound(humanChoice, computerChoice) {
-  console.log(`You chose ${humanChoice}`);
-  console.log(`Computer chose ${computerChoice}`);
+  if (humanChoice === computerChoice) {
+    resultPara.textContent = "It's a tie!";
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    resultPara.textContent = "You win!";
+    humanScore++;
+  } else {
+    resultPara.textContent = "You lose!";
+    computerScore++;
+  }
 }
 
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
+document.querySelectorAll("button").forEach((button) => {
   button.addEventListener("click", () => {
     playRound(button.id, getComputerChoice());
   });
